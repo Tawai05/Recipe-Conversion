edf79eecbf404525e1dc15647b681c51a3ee634d
Tawai <127268250+Tawai05@users.noreply.github.com>
2023-03-08 01:32:05 +0000

    base code from chat gbt

--- main.py
+++ main.py
@@ -6,3 +6,26 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+recipe_name = input("Enter the recipe name: ")
+serving_size = int(input("Enter the serving size: "))
+
+ingredients = []
+
+while True:
+    ingredient = input("Enter an ingredient (or 'done' to finish): ")
+    if ingredient == 'done':
+        break
+    amount = float(input("Enter the amount needed: "))
+    cost_per_unit = float(input("Enter the cost per unit: "))
+    ingredients.append((ingredient, amount, cost_per_unit))
+
+total_cost = sum(amount * cost for _, amount, cost in ingredients)
+cost_per_serving = total_cost / serving_size
+
+print(f"Recipe Name: {recipe_name}")
+print(f"Serving Size: {serving_size}")
+print("Ingredients:")
+for ingredient, amount, cost_per_unit in ingredients:
+    print(f"- {amount} {ingredient} @ ${cost_per_unit:.2f} each")
+print(f"Cost Per Serving: ${cost_per_serving:.2f}")
+print(f"Total Cost: ${total_cost:.2f}")
